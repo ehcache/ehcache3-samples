e54444a8152dc40c5175565d539d68f80b50c0fa
Broszniowski, Aurelien <Aurelien.Broszniowski@terracottatech.com>
2016-09-28 16:32:01 +0200

    added better SoR performance simulation

--- src/main/java/org/terracotta/inno/dao/SoRDao.java
+++ src/main/java/org/terracotta/inno/dao/SoRDao.java
@@ -18,22 +18,50 @@ package org.terracotta.inno.dao;
 import io.rainfall.ObjectGenerator;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Ludovic Orban
  */
 public class SoRDao<T> {
 
+  private final static int INTERVAL_MS = 10;
+  private final static int MAX_TPS = 200000;
+
+  private volatile int tps = 1;
+  private final AtomicInteger loads = new AtomicInteger();
+  private final Timer timer = new Timer(true);
+
   private final ObjectGenerator<T> generator;
   private final Random random = new Random();
 
   public SoRDao(ObjectGenerator<T> generator) {
     this.generator = generator;
+    timer.schedule(new TimerTask() {
+      @Override
+      public void run() {
+        tps = Math.max(MAX_TPS - (loads.getAndSet(0) * 1000 / INTERVAL_MS), 1);
+      }
+    }, INTERVAL_MS, INTERVAL_MS);
   }
 
   public T loadData(Long key) {
+    int latency = 0;
     try {
-      Thread.sleep(random.nextInt(50));
+      loads.incrementAndGet();
+      latency = (Math.max(1000 / tps, 1));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    try {
+      int variance = latency / 10;   // variance = 0.1
+      latency = latency - (variance);
+      latency = latency + random.nextInt(2 * Math.max(variance, 1));
+
+      Thread.sleep(latency);    // sleep between 0.9 x latency and 1.1 x latency
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
     }
