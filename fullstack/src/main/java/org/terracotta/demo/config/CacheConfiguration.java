cf7a724eddc1bf2da73ef454c7abbf5839b3d0f0
Henri Tremblay <henri.tremblay@gmail.com>
2016-11-01 14:46:30 -0400

    Remove Terracotta specific dependencies

--- fullstack/src/main/java/org/terracotta/demo/config/CacheConfiguration.java
+++ fullstack/src/main/java/org/terracotta/demo/config/CacheConfiguration.java
@@ -1,27 +1,17 @@
 package org.terracotta.demo.config;
 
-import org.ehcache.core.EhcacheManager;
-import org.ehcache.core.config.DefaultConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.jcache.JCacheCacheManager;
 import org.springframework.context.annotation.Configuration;
-import org.terracotta.management.model.context.Context;
-import org.terracotta.management.model.stats.ContextualStatistics;
-import org.terracotta.management.model.stats.history.CounterHistory;
 
 import com.codahale.metrics.MetricRegistry;
 
-import java.util.Arrays;
 import java.util.SortedSet;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 @SuppressWarnings("unused")
 @Configuration
