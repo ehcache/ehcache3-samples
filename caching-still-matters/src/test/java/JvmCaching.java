a6bf26e6050be39f638460c373c5145755762648
Henri Tremblay <henri.tremblay@gmail.com>
2018-10-12 13:42:36 -0400

    Show the cache extent a bit more in JvmCaching

--- caching-still-matters/src/test/java/JvmCaching.java
+++ caching-still-matters/src/test/java/JvmCaching.java
@@ -1,13 +1,19 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Demonstrate that the JDK is storing integer from -127 to
+ */
 public class JvmCaching {
 
   @Test
   public void testCache() {
-
-    assertTrue(Integer.valueOf(512) == Integer.valueOf(512));
-
+    for (int i = -140; i < 150 ; i++) {
+      boolean cached = Integer.valueOf(i) == Integer.valueOf(i);
+      System.out.printf("Boxed %-4s cached? %s%n", i, (cached ? "YES" : "No :-("));
+    }
   }
+
 }
