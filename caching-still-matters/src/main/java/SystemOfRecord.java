43f41539c74f790e03f4868b64c7654a09d09a64
Henri Tremblay <henri.tremblay@gmail.com>
2018-10-12 15:23:37 -0400

    Add thundering herd effect

--- caching-still-matters/src/main/java/SystemOfRecord.java
+++ caching-still-matters/src/main/java/SystemOfRecord.java
@@ -4,13 +4,17 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public final class SystemOfRecord {
 
   private static final Logger LOG = LoggerFactory.getLogger(AsideVsPassthrough.class);
+  private static final int MAX_CONNECTIONS = 5;
   private static final SystemOfRecord systemOfRecord = new SystemOfRecord();
 
-  private final Map<String, String> records = new HashMap();
+  private volatile int activeConnections = 0;
+
+  private final Map<String, String> records = new HashMap<>();
 
   private SystemOfRecord() {
     records.put("key", "value");
@@ -18,20 +22,34 @@ public final class SystemOfRecord {
 
   public String load(String key) {
     LOG.warn("Someone is accessing the slow SoR to load : " + key);
+    acquireConnection();
     try {
       TimeUnit.SECONDS.sleep(30);
     } catch (InterruptedException e) {
       // Get out
     }
+    releaseConnection();
     return records.get(key);
   }
 
+  private synchronized void acquireConnection() {
+    if(activeConnections++ >= MAX_CONNECTIONS) {
+      LOG.error("No connections available");
+    };
+  }
+
+  private void releaseConnection() {
+    activeConnections--;
+  }
+
   public void save(String key, String value) {
+    acquireConnection();
     try {
       TimeUnit.SECONDS.sleep(10);
     } catch (InterruptedException e) {
       // Get out
     }
+    releaseConnection();
     records.put(key, value);
   }
 
